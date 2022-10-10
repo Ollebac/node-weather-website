e7700276114d50a1fc7d05ddd63370b278337d29
Ian <iancabello16@gmail.com>
2022-10-10 12:23:28 -0400

    Removed comments

--- src/app.js
+++ src/app.js
@@ -45,19 +45,6 @@ app.get('/help', (req, res) => {
     })
 })
 
-// app.get('/products' , (req, res) => {
-//     if (!req.query.search) {
-//         return res.send({
-//             error: 'You must provide a search'
-//         })
-//     }
-    
-//     console.log(req.query.search);
-//     res.send({
-//         products: []
-//     })
-// })
-
 app.get('/weather', (req, res) => {
     if (!req.query.address) {
         return res.send({
